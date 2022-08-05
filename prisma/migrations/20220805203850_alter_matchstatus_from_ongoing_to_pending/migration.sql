daa1194ab9a6d964cda8f68f078e9fa320f651d8
chon <balint.ducsai@gmail.com>
2022-08-05 22:39:51 +0200

    change matchstatus prisma type

---
+++ prisma/migrations/20220805203850_alter_matchstatus_from_ongoing_to_pending/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [ongoing] on the enum `MatchStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "MatchStatus_new" AS ENUM ('scheduled', 'pending', 'completed', 'canceled');
+ALTER TABLE "Match" ALTER COLUMN "status" TYPE "MatchStatus_new" USING ("status"::text::"MatchStatus_new");
+ALTER TYPE "MatchStatus" RENAME TO "MatchStatus_old";
+ALTER TYPE "MatchStatus_new" RENAME TO "MatchStatus";
+DROP TYPE "MatchStatus_old";
+COMMIT;
