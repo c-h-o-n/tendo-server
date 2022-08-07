7074df9a408b629df3aca448c583030a8c053799
chon <balint.ducsai@gmail.com>
2022-08-07 23:07:41 +0200

    fix cron module error

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MatchService } from 'src/match/match.service';
 import { NotificationModule } from 'src/notification/notification.module';
 import { CronService } from './cron.service';
 
 @Module({
   imports: [NotificationModule],
-  providers: [CronService],
+  providers: [CronService, MatchService],
   exports: [CronService],
 })
 export class CronModule {}
